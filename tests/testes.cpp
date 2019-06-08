26871ef46cde8f0e1aaa6c515619b22c4481f1d4
moraisgabriel <49213929+moraisgabriel@users.noreply.github.com>
2019-06-08 14:38:14 -0300

    Tests 1.0

---
+++ tests/testes.cpp
@@ -0,0 +1,17 @@
+#include "doctest.h"
+
+#include "barbaro.h"
+#include "celula.h"
+#include "guerreiro.h"
+#include "inimigodois.h"
+#include "inimigoum.h"
+#include "ladrao.h"
+#include "mago.h"
+#include "personagem.h"
+
+#include <string>
+
+TEST_CASE("01 - Teste validade do ataque"){
+
+    
+}
