713dd8b94f06925b63c0fcb30726b35145398d16
moraisgabriel <49213929+moraisgabriel@users.noreply.github.com>
2019-06-09 18:38:54 -0300

    Update testes.cpp

--- tests/testes.cpp
+++ tests/testes.cpp
@@ -8,17 +8,18 @@
 #include "ladrao.h"
 #include "mago.h"
 #include "personagem.h"
-
 #include <string>
 
-TEST_CASE("01 - teste construtor celula"){
+TEST_CASE("01 - teste construtor "){
+     int *prt;
+     int a = 5;
+     prt = &a;
      CHECK_NOTHROW(Celula());
-     
+     CHECK_NOTHROW(Personagem(100, 3, 6, 30, "N"));
 
 }
 
 TEST_CASE ("02 - teste retorno"){
-    
 
 }
 
