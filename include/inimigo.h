ab80e4f193b79fd9931135ca2cb4697045692368
Joao <jovicsou@gmail.com>
2019-06-10 14:13:47 -0300

    Implementação Inimigo Um

--- include/inimigo.h
+++ include/inimigo.h
@@ -3,6 +3,8 @@
 
 #include "personagem.h"
 
+#define _ALCANCE_VISAO 2
+
 class Inimigo : public Personagem {
     public:
         Inimigo(int totalHP, int totalMP, int totalSP, int dano, char direcao, Celula* celula);
@@ -10,8 +12,7 @@ class Inimigo : public Personagem {
         /*scanMapa recebe uma célula inicial de referencial na ponta do mapa onde os inimigos começam
         ele então varre todo mapa procurando por um inimigo e compara sua posição com a do inimigo para
         saber se é o mais próximo*/
-        void scanMapa(Celula* inicio);
-        bool ataque(Celula* celula) override;
+        bool scan(Celula** mapa);
 };
 
 #endif
--- src/inimigo.cpp
+++ src/inimigo.cpp
@@ -3,10 +3,23 @@
 Inimigo::Inimigo(int totalHP, int totalMP, int totalSP, int dano, char direcao, Celula* celula):
     Personagem(totalHP, totalMP, totalSP, dano, direcao, celula) {}
 
-void Inimigo::scanMapa(Celula* inicio){
-    for(int i = inicio->getY(); i>=0; i--){
-        for(int j = 0; j < 10; j++){
-            
+bool Inimigo::scan(Celula** mapa){
+    bool personagemEncontrado = false;
+    int i = _celula->getX() - _ALCANCE_VISAO;
+
+    while (i <= _celula->getX() + _ALCANCE_VISAO && !personagemEncontrado){
+        for (int j = _celula->getY() - _ALCANCE_VISAO; j <= _celula->getY() + _ALCANCE_VISAO; j++){
+            if (mapa[i][j].getPersonagem() != nullptr){
+                personagemEncontrado = true;
+                this->movimenta();
+            }
         }
+
+        i++;
     }
+
+    if (personagemEncontrado) //Se algum personagem foi encontrado e a movimentação realizada, retorna true
+        return true;
+    else //Se não, retorna false
+        return false;
 }
