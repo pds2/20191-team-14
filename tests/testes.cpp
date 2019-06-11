0c017be9e7742b8d3d1a41d27e3c90838858cc25
moraisgabriel <49213929+moraisgabriel@users.noreply.github.com>
2019-06-10 22:17:56 -0300

    teste 1.1

--- tests/testes.cpp
+++ tests/testes.cpp
@@ -10,18 +10,19 @@
 #include "personagem.h"
 #include <string>
 
-TEST_CASE("01 - teste construtor "){
+TEST_CASE("01 - teste construtor "){ //testa os construtores das classes
      Celula* celula; 
      CHECK_NOTHROW(Celula());
      CHECK_NOTHROW(Celula(1, 2));
-     CHECK_NOTHROW(Celula('n', 1, 2));
-     CHECK_NOTHROW(Personagem(100, 3, 6, 30, 'n', celula));
-     CHECK_NOTHROW(Barbaro(100, 3, 6, 30, 'n', celula));
-     CHECK_NOTHROW(Guerreiro(100, 3, 6, 30, 'n', celula));
-     CHECK_NOTHROW(Ladrao(100, 3, 6, 30, 'n', celula));
-     CHECK_NOTHROW(Mago(100, 3, 6, 30, 'n', celula));
-     CHECK_NOTHROW(InimigoUm(100, 3, 6, 30, 'n', celula));
-     CHECK_NOTHROW(InimigoDois(100, 3, 6, 30, 'n', celula));
+     CHECK_NOTHROW(Celula('N', 1, 2));
+     CHECK_NOTHROW(Inimigo(100, 3, 6, 30, 'N', celula));
+     CHECK_NOTHROW(Personagem(100, 3, 6, 30, 'N', celula));
+     CHECK_NOTHROW(Barbaro(100, 3, 6, 30, 'N', celula));
+     CHECK_NOTHROW(Guerreiro(100, 3, 6, 30, 'N', celula));
+     CHECK_NOTHROW(Ladrao(100, 3, 6, 30, 'N', celula));
+     CHECK_NOTHROW(Mago(100, 3, 6, 30, 'N', celula));
+     CHECK_NOTHROW(InimigoUm(100, 3, 6, 30, 'N', celula));
+     CHECK_NOTHROW(InimigoDois(100, 3, 6, 30, 'N', celula));
 }
 
 TEST_CASE ("02 - teste retorno"){
@@ -34,24 +35,27 @@ TEST_CASE ("02 - teste retorno"){
      guerreiro.setHP(120);
      CHECK(guerreiro.getHP()==120);
      Barbaro barbaro = Barbaro(100, 3, 6, 30, 'N', celula);;
-     barbaro.setHP(100);
-     CHECK(barbaro.getHP()==100);
+     barbaro.setHP(10);
+     CHECK(barbaro.getHP()==10);
      Ladrao ladrao = Ladrao(100, 3, 6, 30, 'N', celula);
      CHECK(ladrao.getHP()==100);
      Mago mago = Mago(100, 3, 6, 30, 'N', celula);;
      CHECK(mago.getHP()==100);
      InimigoUm inimigoum = InimigoUm(100, 3, 6, 30, 'n', celula);
-     inimigoum.setHP(100);
-     CHECK(inimigoum.getHP()==100);
+     Inimigo inimigo = Inimigo(100, 3, 6, 30, 'N', celula);
+     inimigo.setHP(75);
+     CHECK(inimigo.getHP()==75);
+     inimigoum.setHP(45);
+     CHECK(inimigoum.getHP()==45);
      InimigoDois inimigodois = InimigoDois(100, 3, 6, 30, 'n', celula);
-     inimigodois.setHP;
+     inimigodois.setHP(100);
      CHECK(inimigodois.getHP()==100);
 }
 
 TEST_CASE ("03 - teste ataque"){ 
-     
+
 }
 
 TEST_CASE ("04 - teste habilidade especial"){ 
- 
+     
 }
