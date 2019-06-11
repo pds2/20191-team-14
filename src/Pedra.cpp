e9e13fac856d39a6b2ce7e25195c73a03b5bd9d7
R01z <igorrt66@gmail.com>
2019-06-11 10:57:19 -0300

    Pedra

---
+++ src/Pedra.cpp
@@ -0,0 +1,5 @@
+#include "pedra.h"
+
+
+Pedra::Pedra(int totalHP, int totalMP, int totalSP, int dano, char direcao, Celula* celula) : 
+        Personagem(totalHP, totalMP, totalSP, dano, direcao, celula) {}
